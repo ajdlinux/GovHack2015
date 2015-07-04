4de59a5e133652f019681e583dc50d66ce6d57af
patentstori.es <govhack@patentstori.es>
2015-07-04 02:23:10 +0000

    postgres added as production database

--- patentstories/patentstories/settings.py
+++ patentstories/patentstories/settings.py
@@ -131,7 +131,8 @@ class Production(Base):
 
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'govhack',
+            'USER': 'govhack',
         }
     }
