58fe529f56d906919addf52d4541e494d9532135
Benjamin George Roberts <benjamin.roberts@anu.edu.au>
2015-07-04 10:57:57 +1000

    django configurations added for dev/prod

--- patentstories/patentstories/settings.py
+++ patentstories/patentstories/settings.py
@@ -13,17 +13,14 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+from configurations import Configuration
+from configurations.values import SecretValue
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o-pjjsv$+n9e7(#1ipv99*zluab2u#l=r!_0l@+trov@gw70m_'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -70,18 +67,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'patentstories.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
@@ -100,3 +85,46 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Django Configurations
+
+class Development(Configuration):
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = 'o-pjjsv$+n9e7(#1ipv99*zluab2u#l=r!_0l@+trov@gw70m_'
+
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
+
+    # Database
+    # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+class Production(Configuration):
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = SecretValue()
+
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = False
+
+
+    # Quick-start development settings - unsuitable for production
+    # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
+
+    ALLOWED_HOSTS = ["127.0.0.1"]
+
+
+    # Database
+    # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
