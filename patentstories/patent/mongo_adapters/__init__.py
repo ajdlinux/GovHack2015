aa5437eb9fa463ea08f47a6f0cfa4de78d9ef66b
Benjamin George Roberts <benjamin.roberts@anu.edu.au>
2015-07-04 19:10:25 +1000

    added IPGOD108 adapter

--- patentstories/patent/mongo_adapters/__init__.py
+++ patentstories/patent/mongo_adapters/__init__.py
@@ -2,9 +2,11 @@ __author__ = 'Benjamin George Roberts <benjamin.roberts@anu.edu.au>'
 
 from .IPGOD101 import IPGOD101Adapter
 from .IPGOD107 import IPGOD107Adapter
+from .IPGOD108 import IPGOD108Adapter
 
 # Iterable list of event adapters
 MONGO_PATENT_EVENT_ADAPTERS = [
     IPGOD101Adapter,
     IPGOD107Adapter,
+    IPGOD108Adapter,
 ]
