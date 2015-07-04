9d6cdd045207826f1c423252b17ee87527f4155d
Andrew Donnellan <andrew@donnellan.id.au>
2015-07-04 13:34:07 +1000

    initial set of templates

--- patentstories/patentstories/settings.py
+++ patentstories/patentstories/settings.py
@@ -55,7 +55,7 @@ class Base(Configuration):
     TEMPLATES = [
         {
             'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'DIRS': [],
+            'DIRS': [os.path.join(BASE_DIR, 'templates')],
             'APP_DIRS': True,
             'OPTIONS': {
                 'context_processors': [
