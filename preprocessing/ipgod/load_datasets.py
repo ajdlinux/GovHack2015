a202ea781d7809935206739f75841c5ef6621082
Benjamin George Roberts <benjamin.roberts@anu.edu.au>
2015-07-04 13:29:18 +1000

    set latin-1 as load_dataset encoding and added support for resuming import operations

--- preprocessing/ipgod/load_datasets.py
+++ preprocessing/ipgod/load_datasets.py
@@ -13,7 +13,7 @@ class DataSet:
         :type dataset_file: Path
         """
         self.dataset_name = dataset_path.name.split(".")[0]
-        self.dataset_file = dataset_path.open("r")
+        self.dataset_file = dataset_path.open("r", encoding='latin-1')
         self.column_names = self.dataset_file.readline().strip().split(",")
 
     def load_to_mongo(self):
@@ -26,9 +26,12 @@ class DataSet:
         db = MongoSingleton.get_db()
 
         # Check for existing column and drop
-        if db[self.dataset_name].count() is not 0:
-             print("{0} exists in database, dropping...".format(self.dataset_name))
-             db.drop_collection(self.dataset_name)
+        if db[self.dataset_name].count() == self.__get_remaining_line_count__():
+            print("collection {0} already exists in database, skipping...".format(self.dataset_name))
+            return 0
+        else:
+            print("incomplete collection {0} exists in database, dropping...".format(self.dataset_name))
+            db.drop_collection(self.dataset_name)
 
         collection = db[self.dataset_name]
         entry_count = 0
@@ -47,6 +50,21 @@ class DataSet:
         print("added {0} entries to collection {1}".format(entry_count, self.dataset_name))
 
         return entry_count
+
+    def __get_remaining_line_count__(self):
+        """
+        Gets the number of remaining lines in the file without loosing current position
+        :returns: number of remaning lines
+        :rtype: int
+        """
+        line_count = 0
+        pos = self.dataset_file.tell()
+        while self.dataset_file.readline():
+            line_count += 1
+
+        self.dataset_file.seek(pos)
+
+        return line_count
 
 
 def main():
